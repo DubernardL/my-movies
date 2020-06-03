a60a64c4194dbbf9a54f4bf2e84d8e5977af50fd
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-06-03 16:44:10 +0200

    correction bugs

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,21 +1,21 @@
-const API_TOKEN = 'f69a9d3e37091b340e0252f821875dc0'
+import { TMDB_API_KEY } from 'react-native-dotenv'
 
 export function getFilmsFromApiWithSearchedText (text, page) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + TMDB_API_KEY + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
   .then((response) => response.json())
   .catch((error) => console.log(error))
 }
 
 export function getNewFilmsFromApi(page) {
-  const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + API_TOKEN + '&vote_count.gte=1000&sort_by=release_date.desc&language=fr' + "&page=" + page
+  const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + TMDB_API_KEY + '&vote_count.gte=1000&sort_by=release_date.desc&language=fr' + "&page=" + page
   return fetch(url)
   .then((response) => response.json())
   .catch((error) => console.log(error))
 }
 
 export function getTopRatedFilmsFromApi(page) {
-  const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + API_TOKEN + '&language=fr' + "&page=" + page
+  const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=' + TMDB_API_KEY + '&language=fr' + "&page=" + page
   return fetch(url)
   .then((response) => response.json())
   .catch((error) => console.log(error))
@@ -26,7 +26,7 @@ export function getImageFromApi(name) {
 }
 
 export function getFilmDetailFromApi(id) {
-  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + TMDB_API_KEY + '&language=fr')
     .then((response) => response.json())
     .catch((error) => console.error(error));
 }
